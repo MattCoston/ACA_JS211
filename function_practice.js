de37a4363e4db38b9d7b0be4f7db2b0ccc89f801
Matt Coston <mattcoston7@gmail.com>
2020-07-19 15:04:58 -0500

    function practice

---
+++ function_practice.js
@@ -0,0 +1,16 @@
+const name = "Matt"
+const userAge = 35
+
+const dividedBy = (num1, num2) => {
+    let division = num1 / num2
+    return division
+
+}
+
+const personAge = (name, age) => {
+    console.log(`${name} is ${age} centuries old`)
+
+}
+
+console.log(dividedBy(4, 2))
+personAge(name, userAge)
