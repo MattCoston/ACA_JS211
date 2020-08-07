946e098fc1a70d717118e98cb740c2b7f4345535
Matt Coston <mattcoston7@gmail.com>
2020-08-06 20:47:41 -0500

    for loops practice

---
+++ Week2/forLoop.js
@@ -0,0 +1,17 @@
+
+//for(let i = 2; i<=10;i=i+2){
+//    console.log(i)
+//}
+
+
+let friends = [
+    {name: "Frank", favColor: "aqua"},
+    {name: "Fran", favColor: "teal"},
+    {name: "Murica", favColor: "blue"},
+    {name: "Test", favColor: "black"}
+]
+//console.log(friends)
+for(i = 0; i < friends.length; i+=1){
+    console.log("Hi " + friends[i].name + " I know your favorite color is " + friends[i].favColor + " but have you considered Grey?!")
+}
+
