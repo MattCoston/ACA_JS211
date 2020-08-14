54f7583ac6514d60e90bb6ab65700b0c70769fb6
Matt Coston <mattcoston7@gmail.com>
2020-08-13 19:47:55 -0500

    Added js file to week 3

---
+++ Week3/foreach.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let cars = ["bmw", "audi", "tesla"]
+
+//for(let i = 0; i < cars.length; i++){
+ //   console.log(cars[i])
+//}
+
+cars.forEach(function(car){
+    console.log(car)
+});
