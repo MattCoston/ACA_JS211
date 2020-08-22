3d867d1a0168b61c1c5927348a61c935ee5275ac
Matt Coston <mattcoston7@gmail.com>
2020-08-22 12:15:18 -0500

    Loops assigment started

---
+++ Loops_Assignment/main.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// let i = 0
+// let count = 0
+// do {
+//     count = i += 1
+
+// }  while(i < 1000)
+// console.log(count)
+
+// let person = {
+//     firstName: "Jane",
+//     lastName: "Doe",
+//     birthDate: "Jan 5, 1925",
+//     gender: "female"
+// }
+
+// let keyFinder = (obj) =>{
+//     console.log(Object.keys(obj))
+// }
+
+// keyFinder(person)
+
+// let objValueFinder = (obj) => {
+//     console.log(Object.entries(obj))
+// }
+
+// objValueFinder(person)
+
+let arrayOfPersons = [
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    birthDate: "Jan 5, 1925",
+    gender: "female",
+  },
+
+  {
+    firstName: "John",
+    lastName: "Smith",
+    birthDate: "Jan 6, 1925",
+    gender: "male",
+  },
+  {
+    firstName: "Gomer",
+    lastName: "Pyle",
+    birthDate: "July 5, 1945",
+    gender: "male",
+  },
+];
+
+console.log(arrayOfPersons);
