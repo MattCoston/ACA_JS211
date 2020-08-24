7fe81685f7009c204671cab1702e26bbc04148ec
Matt Coston <mattcoston7@gmail.com>
2020-08-24 18:18:35 -0500

    TOH WORK

--- TowersOfHanoi/main.js
+++ TowersOfHanoi/main.js
@@ -1,34 +1,36 @@
-'use strict';
+'use strict'
 
-const assert = require('assert');
-const readline = require('readline');
-const { truncate } = require('fs');
-const { start } = require('repl');
+const assert = require('assert')
+const readline = require('readline')
+const { truncate } = require('fs')
+const { start } = require('repl')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
-
+// establishes a stack object with 3 arrays in it.
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
-};
-
+}
+// made stacks into variables for ease of use.
 let a = stacks.a
 let b = stacks.b
 let c = stacks.c
 
-
+// This function prints the board
 const printStacks = () => {
-  console.log("a: " + stacks.a);
-  console.log("b: " + stacks.b);
-  console.log("c: " + stacks.c);
+  console.log("a: " + stacks.a)
+  console.log("b: " + stacks.b)
+  console.log("c: " + stacks.c)
 }
 
 const movePiece = (startStack, endStack) => {
   // Need to move one piece from origin stack to destination stack
+  // I changed the name of these variables within the function for my own sanity's sake. 
+  // This will still work fine with using startStack and endStack
   let originMove = startStack
   let destinationMove = endStack
   if(originMove == 'a' &&  destinationMove == 'b'){
@@ -51,22 +53,67 @@ const movePiece = (startStack, endStack) => {
     return stacks
   }
 }
+/* This function should evaluate if a move is able to be made or not.  You should not be able to put a larger number on top of a smaller number.  4 cannot go on top of a 2 in the given array.  
 
-const isLegal = () => {
-  // Your code here
+Each if statement takes in the start and end stack the user gives.  It evaluates if the last element of the A,B or C array is greater than the endStack that it is trying to push on to.  If the element is greater than it does not allow the move.*/
+const isLegal = (startStack, endStack) => {
+  let lastElementA = a[a.length - 1]
+  let lastElementB =b[b.length - 1]
+  let lastElementC = c[c.length - 1]
+
+    if (startStack == 'a' && endStack == 'b'){
+        if(lastElementA > lastElementB){
+          return false 
+        } else {
+          return true
+        }
+    } if (startStack == 'b' && endStack == 'c'){
+        if(lastElementB > lastElementC){
+          return false 
+        }  else {
+          return true
+        }
+    } if (startStack == 'a' && endStack == 'c'){
+      if(lastElementA > lastElementC){
+        return false 
+      } else {
+        return true
+      }
+    } if (startStack == 'c' && endStack == 'a'){
+      if(lastElementC > lastElementA){
+        return false 
+      } else {
+        return true
+      }
+    } if (startStack == 'b' && endStack == 'a'){
+      if(lastElementB > lastElementA){
+        return false 
+      } else {
+        return true
+      }
+    } if (startStack == 'c' && endStack == 'b'){
+      if(lastElementC > lastElementA){
+        return false 
+      } else {
+        return true
+      }
+    } if(startStack === endStack){
+      return false
+    } else {
+      return false
+    }
+
   
 
 }
-
+let errorMessage = "********** Not allowed **********"
 const checkForWin = () => {
   /* if statment that equates to if the length of b or c is equal to 4 return true and console log the win statement
   otherwise return false*/
-  if (b.length === 4)
-  {
+  if (b.length === 4){
       console.log("Congrats you have won!")
       return true
-  } else if (c.length === 4)
-  {
+  } else if (c.length === 4){
         console.log("Congrats you have won!")
         return true
   } else {
@@ -75,18 +122,26 @@ const checkForWin = () => {
 }
 
 const towersOfHanoi = (startStack, endStack) => {
-  movePiece(startStack, endStack)
+  if (isLegal(startStack, endStack) == true){
+    movePiece(startStack, endStack)
+    
+  } else{
+    console.log(errorMessage)
+  
+  }
   checkForWin()
 }
-
 const getPrompt = () => {
-  printStacks();
+  printStacks()
   rl.question('start stack: ', (startStack) => {
     rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
-      getPrompt();
-    });
-  });
+      towersOfHanoi(startStack, endStack)
+      getPrompt()
+    })
+  })
 }
 
-getPrompt();
+getPrompt()
+
+
+/* Unit TESTS WILL GO HERE TOMORROW */
