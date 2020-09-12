92ee557cf65b70a5f0b3f02ec333ac8dc0df63c4
Matt Coston <mattcoston7@gmail.com>
2020-09-12 16:43:37 -0500

    Worked on Fetch Assignment

--- Week7/main.js
+++ Week7/main.js
@@ -1,15 +1,15 @@
-console.log("JS LOADED")
+console.log("JS LOADED");
 
 
-console.log("about to make fetch call")
-let fetchPromise = window.fetch("https://randomuser.me/api/?results=5")
+console.log("about to make fetch call");
+let fetchPromise = window.fetch("https://randomuser.me/api/?results=5");
 let jsonPromise = fetchPromise.then(function(data){
-    console.log("Fetch call finished", data)
-    return data.json()
+    console.log("Fetch call finished", data);
+    return data.json();
 })
 jsonPromise.then(function(json){
-    console.log("Have the actual payload", json)
-    process(json.results)
+    console.log("Have the actual payload", json);
+    process(json.results);
     
 })
 
@@ -19,29 +19,34 @@ function process(users){
     let ul = document.getElementById("people")
 
     users.forEach(function(user){
-        console.log("User First Name", user.name.first)
+        console.log("User First Name", user.name.first);
 
-        let li = document.createElement("li")
+        let li = document.createElement("li");
         ul.appendChild(li)
 
-        let img = document.createElement("img")
+        let img = document.createElement("img");
         li.appendChild(img)
-        let picture = user.picture.large
-        img.src = picture
-        //todo: go to set the src on the image tag
+        let picture = user.picture.large;
+        img.src = picture;
 
-        let nameDiv = document.createElement("div")
+        let nameDiv = document.createElement("div");
         li.appendChild(nameDiv)
-        //todo: got to set the text on the name div
-        nameDiv.innerHTML = user.name.first + " " + user.name.last
-        let addressDiv = document.createElement("div")
+        nameDiv.innerHTML = user.name.first + " " + user.name.last;
+        let addressDiv = document.createElement("div");
         li.appendChild(addressDiv)
         //todo: got to set the text inside the address div
-        addressDiv.innerHTML = user.location.street.number + " " + user.location.street.name
+        addressDiv.innerHTML = user.location.street.number + " " + user.location.street.name + " " + user.location.city + " " + user.location.country;
         li.addEventListener("click", function(){
+            console.log("this event works")
+            let hide = addressDiv
+            if ( hide.style.display == "none"){
+                hide.style.display = "block"
+            } else {
+                hide.style.display = "none"
+            }
             //have to add a listener to the li, so when the name div is clicked
             //if the address div is hidden, then it should change to showing
             // if it is showing already then hide it
-        })
-    })
+        });
+    });
 }
